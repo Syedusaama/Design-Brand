c5361e7481b25b6c4c0617967ebbc53a5e789479
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-05-24 21:02:46 +0500

    update

--- term-and-conditions.php
+++ term-and-conditions.php
@@ -7,24 +7,41 @@
 <!-- Main Header -->
 <?php include "./components/header.php"?>
 <!-- Main Header -->
+<!-- Banner -->
+<section class="inner-banner5">
+    <div>
+    <div class="inner-bann-heading">
+        <div class="row">
+        <div class="col-md-8 inner-bann-heading">
+                <h1>Transform Your Concepts into Captivating Stories with Our Professional <span class="subheading-clr">Video Animation </span>Services</h1>
+            </div>
+            <div class="col-md-4">
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+<!-- Banner -->
 
-
-<section>
+<section class="term-background">
 <div class="mt-50 mb-100">
-			<div class="container">
-				<div class="row content-area-com">
-					<h5>TERMS & CONDITIONS</h5> The following terms and conditions apply to projects undertaken by <a href="#">www.globalpublishingagency.com</a>
+			<div class="container content-area-com">
+				<div class="row ">
+					<h5>TERMS & CONDITIONS</h5>
+                    <p>The following terms and conditions apply to projects undertaken by <a href="#">www.globaldesignagency.com</a></p>
+                     
 					<h5>COPYRIGHT </h5>You will hold full copyright to the content that we will develop for you and deliver as per the agreed terms. We will hold no copyright whatsoever on the content and you are free to publish and use it as per your wish.
 					<h5> PAYMENT</h5> We charge 100% advance payment for all services mentioned on the website. All transaction charges and tax (as applicable) are to be borne by the client.
-					<h5> REVISIONS</h5>We offer unlimited number of revisions on delivered work, as long as the requests are made within 7 days of delivering the work, and in special cases, as long as the requests are made within 14 days of delivering of work. All revision requests pertaining to customers order will be completed as per the urgency of the order with 24 hours as standard minimum turnaround time.<br><br>Revision turnaround time as per urgency is as follows:<br><br><ul><li>For orders up to 24 hours of urgency; Revision turnaround time would be 24 hours.</li><li>For orders between 24-48 hours of urgency; Revision turnaround time would be 48 hours.</li><li>For orders of 48 hours and above; Revision turnaround time would be 72 hours.</li></ul>
+					<h5> REVISIONS</h5>We offer unlimited number of revisions on delivered work, as long as the requests are made within 7 days of delivering the work, and in special cases, as long as the requests are made within 14 days of delivering of work. All revision requests pertaining to customers order will be completed as per the urgency of the order with 24 hours as standard minimum turnaround time.<br><br>Revision turnaround time as per urgency is as follows:<br><br><ul class="ul-tabss"><li><i class="fa-solid fa-check"></i>For orders up to 24 hours of urgency; Revision turnaround time would be 24 hours.</li><li><i class="fa-solid fa-check"></i>For orders between 24-48 hours of urgency; Revision turnaround time would be 48 hours.</li><li><i class="fa-solid fa-check"></i>For orders of 48 hours and above; Revision turnaround time would be 72 hours.</li></ul>
 					<h5>PROJECT REQUIREMENTS</h5> Any project requirements will be discussed with the clients before the start of a project. Service level disputes will be addressed based on the agreed terms and requirements at the beginning of a project, these terms and conditions, as well as reasonable business practices.
 					<h5>REFUND POLICY</h5> You may claim a full refund of your amount before we have started working on your project. However, once the work has started on a project the refund will be processed after deducting any applicable transaction charges from the amount. Refund requests will not be accepted if you sign up using a limited time or discounted promotion.<br><br>A refund request can be submitted if you are not satisfied with any product or service provided by Amazon Publishing Agency within 5 days of your order completion. Certain percentage based refund would be granted if we are unable to provide required service through our unlimited revision policy. If no refund request is received within five days after your order has been delivered, your satisfaction over the delivered order shall be assumed by both the parties.
-					<h5>TERM OF USE OF THIS WEBSITE</h5><ul>
-						<li>The information provided on www.amazonpublishingagency.com includes, but is not limited to, the services provided by the company and does not render any advice, certifications, guarantees or warranties.</li><br>
-						<li>The company or any of its affiliates or associates or employees shall not be in any way responsible for any loss or damage that may arise to any person from any inadvertent error in the information contained in this website.</li><br>
-						<li>The company and its affiliates and associates shall not be liable, at any time, for any failure of performance, error, omission, interruption, deletion, defect, delay in operation or transmission, computer virus, communications line failure, theft or destruction or unauthorized access to, alteration of, or use of information contained on the website.</li><br>
-						<li>You agree to defend, indemnify, and hold the company and its subsidiaries, affiliates, officers, agents, and employees, harmless from and against any claims, liabilities, damages, losses, and expenses, arising out of or in any way connected with (i) your access to or use of the website and the services therein; (ii) your violation of these Terms of Use; (iii) your violation of any third party right, including without limitation any intellectual property right, publicity, confidentiality, property or privacy right.</li><br>
-						<li>This Agreement or your use of this Site does not constitute any joint venture, partnership, employment or agency relationship between you and the company.</li><br>
+					<h5>TERM OF USE OF THIS WEBSITE</h5>
+                    <ul class="ul-tabss">
+						<li><i class="fa-solid fa-check"></i>The information provided on www.amazonpublishingagency.com includes, but is not limited to, the services provided by the company and does not render any advice, certifications, guarantees or warranties.</li><br>
+						<li><i class="fa-solid fa-check"></i>The company or any of its affiliates or associates or employees shall not be in any way responsible for any loss or damage that may arise to any person from any inadvertent error in the information contained in this website.</li><br>
+						<li><i class="fa-solid fa-check"></i>The company and its affiliates and associates shall not be liable, at any time, for any failure of performance, error, omission, interruption, deletion, defect, delay in operation or transmission, computer virus, communications line failure, theft or destruction or unauthorized access to, alteration of, or use of information contained on the website.</li><br>
+						<li><i class="fa-solid fa-check"></i>You agree to defend, indemnify, and hold the company and its subsidiaries, affiliates, officers, agents, and employees, harmless from and against any claims, liabilities, damages, losses, and expenses, arising out of or in any way connected with (i) your access to or use of the website and the services therein; (ii) your violation of these Terms of Use; (iii) your violation of any third party right, including without limitation any intellectual property right, publicity, confidentiality, property or privacy right.</li><br>
+						<li><i class="fa-solid fa-check"></i>This Agreement or your use of this Site does not constitute any joint venture, partnership, employment or agency relationship between you and the company.</li><br>
 						The representatives may use the pseudo names depending on which region they are interacting in order to facilitate and do a better communication with our customers because we deal in the global market.
 					</ul>
 				</div>

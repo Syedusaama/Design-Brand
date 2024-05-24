ea918526057ca69b8400b6a24280c0a7dece324b
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-05-24 20:10:25 +0500

    update

---
+++ privacy-policy.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "./components/head.php"?>
+</head>
+<body>
+<!-- Main Header -->
+<?php include "./components/header.php"?>
+<!-- Main Header -->
+
+
+
+<section>
+<div class="mt-50 mb-100">
+			<div class="container">
+				<div class="row content-area-com">
+					<h5>PRIVACY POLICY</h5>
+					<p> Privacy policy helps you and us both in keeping your information confidential. Our privacy policy will help in understanding the process we follow to maintain your privacy during and after the work process.
+						</p>
+					<h5>USER INFORMATION</h5>
+					<p> We may collect the following information for your identification and serene collaboration.
+						<ul>
+							<li>Name, business name and job title.</li>
+							<li>Contact information including phone numbers, email address or website.</li><br>
+						</ul>
+						<p>We require this information to understand your needs and provide you with a better service,<br> and in particular for the following reasons:</p>
+						<ul>
+							<li>We may use the information to improve our products and services.</li>
+							<li>We may also use your information to contact you to provide feedback on our services.</li>
+							<li>We may use the information to customize the website according to your interests.</li>
+						</ul>	
+						</p>
+					<h5> SECURITY</h5>
+					<p>We are committed to ensuring that your information is secure. In order to prevent unauthorized access or disclosure, we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.
+						<ul>
+							<li>Information of clients at Amazon Publishing Agency is kept extremely secure during transmission by the use of the Secure Sockets Layer (SSL) Software which encrypts information that client puts in.</li>
+							<li>Amazon Publishing Agency follows generally accepted industry standards to protect the personal information that is being submitted by the client during transmission and once we receive it. However, since no method of transmission over the Internet, or method of electronic storage, is 100% secure, we despite using commercially acceptable means to protect your personal information cannot guarantee its absolute security.</li>
+						</ul>
+					</p>
+					<h5>3RD PARTY SHARING</h5>
+					<p> 						
+						<ul>
+							<li>We do not divulge any Personal information to the third parties.</li>
+							<li>In no circumstances do we provide or sell personal information of our clients to the third parties.</li>
+							<li>Amazon Publishing Agency utilizes services of credit card processing companies to bill you for services. These companies never share, store, save or use exclusive information for any other purposes.</li>
+						</ul>
+					</p>
+					<h5>COOKIES AND THEIR USE</h5>
+					<p>
+						<ul>
+							<li>Cookies, alphanumeric identifiers, facilitate our systems to recognize the clients' browsers and storage of items in their Shopping Carts during visits.</li><br>
+							<li>To prevent the browser from accept new cookies, disable cookies and notify the user when a new cookie is received, Help portion of the toolbar on most browsers becomes most instrumental.</li><br>
+							<li>Normally clients do not restrict cookies as they allow you to take full advantage of the top features at our site. Our study suggests that the clients leave cookies to be accepted.</li><br>
+							<li>Amazon Publishing Agency keeps track of your IP address to help diagnose problems with our server and to administer our website. Your IP address is also used to gather broad demographic information about you, such as your location and your Internet service provider.</li><br>
+							<li>Client's collective information on how our users are utilizing the site may be gathered. This information might include details vis-à-vis traffic patterns through the site and search queries. No IP address/log file information is tied to Personally Identifiable Information (PII).</li><br>
+							<li>Information regarding browser types, access times, URLs through which people visit our site and URLs viewed by visitors while on our site is not accessible third parties, except in combined form.</li><br>
+						</ul>
+						In order to ensure the privacy of our valued customer we never store any sort of information related to our clients neither we do take any cards information from client.
+					</p>
+					<h5>CONSUMER DATA SAFETY MEASURES</h5>
+					<p>We respect your right of privacy. We therefore do not share your name and contact details with any third party. Besides, we keep information provided by you as confidential. The information you provide are used only to understand your requirements and to improve our services to suit your needs rather effectively. We ensure to follow the standards set by PCI and consumer data protection.<br><br>It is hereby declared that our company does not authorize its representatives to collect any confidential information from our customers i.e. credit card information etc. Accordingly we advise our customers not to share any confidential and sensitive nature information with our employees. If you do so; you shall be doing at your own risk and our company shall not be liable for any misuse.<br><br>Your ordered work may be forwarded to any of our production or service centers located globally, for the quality delivery. However, they are also bound by an NDA to keep customer’s information as highly confidential.</p>
+					<h5>PSEUDONYM POLICY</h5>
+					<p>We hold a policy of using the pseudonym to ensure the following:<br><br>With a view to avoid undue confusion and hassle at our customers end when they are assigned a new account manager, we use pseudonyms; it helps our customers remain connected with a single point of contact and remember their name comfortably.<br><br>Moreover; as our staff comprises of people from different regions and cultures, hence the use of pseudonym helps us present a uniform culture of the organization</p>
+					<h5>1. INTRODUCTION</h5>
+					<p>This Privacy Policy explains how Global Publishing Agency collects, uses, and protects the email addresses and mobile numbers provided by users on our website. We are committed to ensuring that your privacy is protected and that we comply with all relevant data protection laws.</p>
+					<h5>2. INFORMATION WE COLLECT</h5>
+					<p>We may collect the following information:
+						<ul>
+							<li>Mobile phone numbers</li>
+							<li>Any additional information provided voluntarily by the user.</li>
+						</ul>
+					</p>
+					<h5>3. HOW WE USE THE INFORMATION</h5>
+					<p>We use the collected mobile numbers for the following purposes:
+						<ul>
+							<li>To send relevant updates related to our services.</li>
+							<li>To notify users about account-related information.</li>
+							<li>For internal record keeping.</li>
+						</ul>
+					</p>
+					<h5>4. CONSENT FOR MOBILE NUMBER COLLECTION</h5>
+					<p>By providing your mobile number on Global Publishing Agency, you consent to the collection and use of this information as outlined in this Privacy Policy.</p>
+					<h5>5. SECURITY</h5>
+					<p>We are committed to ensuring that your information is secure. To prevent unauthorized access or disclosure, we have put in place suitable physical, electronic, and managerial procedures to safeguard and secure the information we collect online.</p>
+					<h5>6. CONTROLLING YOUR PERSONAL INFORMATION</h5>
+					<p>You may choose to restrict the collection or use of your mobile number in the following ways:
+						<ul>
+							<li>If you have previously agreed to us using your mobile number for direct marketing purposes, you may change your mind at any time by contacting us at marketing@globalpublishingagency.com.or you can reply “STOP” or “Unsubscribe” to stop receiving the messages</li>
+						</ul>
+					</p>
+					<h5>7. SHARING YOUR INFORMATION</h5>
+					<p>We will not sell, distribute, or lease your personal information to third parties unless we have your permission or are required by law to do so.</p>
+					<h5>8. YOUR RIGHTS</h5>
+					<p>You have the right to request details of the personal information we hold about you. If you would like a copy of this information, please contact us at marketing@globalpublishingagency.com.</p>
+					<h5>9. CHANGES TO THIS POLICY</h5>
+					<p>Global Publishing Agency may change this policy from time to time by updating this page. You should check this page periodically to ensure that you are comfortable with any changes.</p>
+				</div>
+			</div>
+</div>
+</section>
+
+
+
+
+
+<!-- Contact Section -->
+<?php include "./components/contact.php"?>
+<!-- Contact Section -->
+
+
+<!-- Footer Section -->
+<?php include "./components/footer.php"?>
+<!-- Footer Section -->
+
+<script src="./assets/js/jquery.js"></script>
+<script src="./assets/js/custom.js"></script>
+<script>
+    new WOW().init();
+</script>
+<script src="https://cdn.lordicon.com/lordicon.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>

8a91b0f930cf0ad96f5fe4a5abca8fff374fe746
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-05-27 19:05:27 +0500

    update

--- logo-design.php
+++ logo-design.php
@@ -10,59 +10,41 @@
 
 <section class="inner-banner2">
     <div>
-    <div class="inner-bann-heading">
-        <div class="row align-items-center">
-            <div class="col-md-7 inner-bann-heading">
-                <h1>Crafting Iconic <span class="subheading-clr">Logo</span> That Define Your Brand</h1>
-            </div>
-            <div class="col-md-5">
-            <div class="form_banner">
-
-<form class="form-get-quote" action="javascript:void(0)">
-
-    <h3>We will always provide the best service.</h3>
-
-    <div class="grp">
-
-        <input type="text" name="quote[name]" placeholder="Your Name" required="">
-
-    </div>
-
-    <div class="grp">
-
-        <input type="email" name="quote[email]" placeholder="Email Address" required="">
-
-    </div>
-
-    <div class="grp">
-
-        <input type="text" name="quote[phone]" placeholder="Phone Number" required="">
-
-    </div>
-
-    <div class="grp">
-
-        <textarea name="quote[message]" placeholder="I am looking for..."></textarea>
-
-    </div>
-
-    <div class="grp">
-
-        <button class="btn-title">Submit</button>
-
-    </div>
-
-</form>
-
-</div>
+        <div class="inner-bann-heading">
+            <div class="row align-items-center">
+                <div class="col-md-7 inner-bann-heading">
+                    <h1>Crafting Iconic <span class="subheading-clr">Logo</span> That Define Your Brand</h1>
+                </div>
+                <div class="col-md-5">
+                    <!-- form -->
+                <div class="form_banner">
+                <form class="form-get-quote" action="javascript:void(0)">
+                    <h3>We will always provide the best service.</h3>
+                    <div class="grp">
+                        <input type="text" name="quote[name]" placeholder="Your Name" required="">
+                    </div>
+                    <div class="grp">
+                        <input type="email" name="quote[email]" placeholder="Email Address" required="">
+                    </div>
+                    <div class="grp">
+                        <input type="text" name="quote[phone]" placeholder="Phone Number" required="">
+                    </div>
+                    <div class="grp">
+                        <textarea name="quote[message]" placeholder="I am looking for..."></textarea>
+                    </div>
+                    <div class="grp">
+                        <button class="btn-title">Submit</button>
+                    </div>
+                </form>
+                </div>
+                <!-- form -->
+                </div>
             </div>
         </div>
     </div>
-</div>
 </section>
-<!-- Banner -->
 
-<!-- First Section -->
+<!-- Banner -->
 <!-- First Section -->
 <section class="inner-first-secbanner">
     <div class="container py-5">

1347cfdf9fc64d0828545afc2b82e68abdafd398
Syed Usama <68234611+Syedusaama@users.noreply.github.com>
2024-05-30 16:01:56 +0500

    update

---
+++ book-cover-design.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "./components/head.php"?>
+</head>
+<body>
+<!-- Main Header -->
+<?php include "./components/header.php"?>
+<!-- Main Header -->
+
+<section class="inner-banner2">
+    <div>
+        <div class="inner-bann-heading">
+            <div class="row align-items-center">
+                <div class="col-md-7 inner-bann-heading">
+                    <h1>Crafting Iconic <span class="subheading-clr">Logo</span> That Define Your Brand</h1>
+                    <p>At Global Design Agency, we specialize in creating iconic logos that define your brand identity. Our expert designers create visually stunning and memorable logos that connect your brand with your audience and help you stand out from the competition. Let us help you make a lasting first impression.</p>
+                </div>
+                <div class="col-md-5">
+                    <!-- form -->
+                <div class="form_banner">
+                <form class="form-get-quote" action="javascript:void(0)">
+                    <h3>We will always provide the best service.</h3>
+                    <div class="grp">
+                        <input type="text" name="quote[name]" placeholder="Your Name" required="">
+                    </div>
+                    <div class="grp">
+                        <input type="email" name="quote[email]" placeholder="Email Address" required="">
+                    </div>
+                    <div class="grp">
+                        <input type="text" name="quote[phone]" placeholder="Phone Number" required="">
+                    </div>
+                    <div class="grp">
+                        <textarea name="quote[message]" placeholder="I am looking for..."></textarea>
+                    </div>
+                    <div class="grp">
+                        <button class="btn-title">Submit</button>
+                    </div>
+                </form>
+                </div>
+                <!-- form -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Banner -->
+<!-- First Section -->
+<section class="inner-first-secbanner">
+    <div class="container py-5">
+        <div class="row align-items-center">
+            <div class="col-md-6 innerban-content">
+                <h2>Logo Design</h2>
+                <p>Capture the essence of your brand with our bespoke logo design service. In today's dynamic market, a distinctive logo is the cornerstone of your brand identity. Our seasoned team collaborates closely with you to understand your vision, values, and target audience. Whether you're a burgeoning startup or a seasoned corporation, we tailor our approach to suit your unique requirements. From minimalist and modern to intricate and illustrative, we breathe life into your brand through captivating visuals. Prioritizing originality and creativity, we ensure that your logo stands out amidst competitors. With meticulous attention to detail and a commitment to excellence, we deliver timeless designs that resonate with your audience. Elevate your brand identity with our unparalleled logo design service. Let's embark on this creative journey together.</p>
+                <div class="col">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <a href="#" class="header-btn">Let’s Talk</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 innerpage-rightimg">
+            <img src="./assets/images/services/first-section/logo.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- First Section -->
+
+<!-- Second Section -->
+<section class="inner-second-secbanner">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 text-center py-5">
+                <h2 class="title-heading">Logo Portfolio</h2>
+                <p class="title-p">In the virtual world teeming with competition, we stand out with our fair pricing model, prioritizing quality over quantity in every order. <br>Your satisfaction is our driving force, ensuring excellence with every project.</p>   
+            </div>
+            <div class="portfolio-items-box">
+            <div class="gallery">
+                <img src="./assets/images/portfolio/logo/l1.png" alt="a forest after an apocalypse">
+                <img src="./assets/images/portfolio/logo/l2.png" alt="a great view of the sea above the mountain">
+                <img src="./assets/images/portfolio/logo/l3.png" alt="a great view of the sea above the mountain">
+                <img src="./assets/images/portfolio/logo/l4.png" alt="a waterfall and many rocks">
+                <img src="./assets/images/portfolio/logo/l5.png" alt="sime pink flowers">
+                <img src="./assets/images/portfolio/logo/l6.png" alt="a cool landscape">
+                <img src="./assets/images/portfolio/logo/l7.png" alt="inside a town between two big buildings">
+                <img src="./assets/images/portfolio/logo/l8.png" alt="a great view of the sea above the mountain">
+                <img src="./assets/images/portfolio/logo/l9.png" alt="a house on a mountain">
+                <img src="./assets/images/portfolio/logo/l10.png" alt="a house on a mountain">
+                <img src="./assets/images/portfolio/logo/l11.png" alt="a house on a mountain">
+                <img src="./assets/images/portfolio/logo/l12.png" alt="a house on a mountain">
+              </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+<!-- Second Section -->
+
+<!-- Third section-->
+<section class="inner-third-secbanner">
+    <div class="container" >
+        <div class="row">
+    <div class="text-center pb-5">
+        <h2 class="title-heading">Affordable Packages</h2>
+        <p class="title-p">We are well aware of the competition. That is the reason why we have a fair pricing<br> model. We ensure quality over quantity on each order.</p>   
+        </div>
+            <ul class="project-items">
+                <li>
+                    <div class="project-item-box">
+                        <div class="project-item-img">
+                            <img src="assets/images/tabs-img.png" alt="">
+                        </div>
+                        <div class="project-item-body">
+                            <h5>Instent Basic </h5>
+                            <h4>$99</h4>
+                            <ul class="tabs-ul-01">
+                            <li><i class="fa-solid fa-check"></i>1 Logo Design Concepts</li>
+                            <li><i class="fa-solid fa-check"></i>By 1 Experienced Designer</li>
+                            <li><i class="fa-solid fa-check"></i>2 Free Revisions</li>
+                            <li><i class="fa-solid fa-check"></i>Dedicated Project Manager</li>
+                            <li><i class="fa-solid fa-check"></i>24-48 Hours Delivery</li>
+                            <li><i class="fa-solid fa-check"></i>1 Finalized Logo</li>
+                            <li><i class="fa-solid fa-check"></i>100% Ownership</li>
+                            <li><i class="fa-solid fa-check"></i>Online Jpeg Files</li>
+                            <li><i class="fa-solid fa-check"></i>No Print Ready PDF File </li>
+                             </ul>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="project-item-box">
+                        <div class="project-item-img">
+                            <img src="assets/images/tabs-img.png" alt="">
+                        </div>
+                        <div class="project-item-body">
+                            <h5>Instent Basic </h5>
+                            <h4>$99</h4>
+                            <ul class="tabs-ul-01">
+                            <li><i class="fa-solid fa-check"></i>1 Logo Design Concepts</li>
+                            <li><i class="fa-solid fa-check"></i>By 1 Experienced Designer</li>
+                            <li><i class="fa-solid fa-check"></i>2 Free Revisions</li>
+                            <li><i class="fa-solid fa-check"></i>Dedicated Project Manager</li>
+                            <li><i class="fa-solid fa-check"></i>24-48 Hours Delivery</li>
+                            <li><i class="fa-solid fa-check"></i>1 Finalized Logo</li>
+                            <li><i class="fa-solid fa-check"></i>100% Ownership</li>
+                            <li><i class="fa-solid fa-check"></i>Online Jpeg Files</li>
+                            <li><i class="fa-solid fa-check"></i>No Print Ready PDF File </li>
+                             </ul>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="project-item-box">
+                        <div class="project-item-img">
+                            <img src="assets/images/tabs-img.png" alt="">
+                        </div>
+                        <div class="project-item-body">
+                            <h5>Instent Basic </h5>
+                            <h4>$99</h4>
+                            <ul class="tabs-ul-01">
+                            <li><i class="fa-solid fa-check"></i>1 Logo Design Concepts</li>
+                            <li><i class="fa-solid fa-check"></i>By 1 Experienced Designer</li>
+                            <li><i class="fa-solid fa-check"></i>2 Free Revisions</li>
+                            <li><i class="fa-solid fa-check"></i>Dedicated Project Manager</li>
+                            <li><i class="fa-solid fa-check"></i>24-48 Hours Delivery</li>
+                            <li><i class="fa-solid fa-check"></i>1 Finalized Logo</li>
+                            <li><i class="fa-solid fa-check"></i>100% Ownership</li>
+                            <li><i class="fa-solid fa-check"></i>Online Jpeg Files</li>
+                            <li><i class="fa-solid fa-check"></i>No Print Ready PDF File </li>
+                             </ul>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+    </div>
+    </div>
+</section>
+<!-- Third section-->
+
+<!-- Work section-->
+<?php include "./components/work.php"?>
+<!-- Work section-->
+
+<!-- Contact Section -->
+<?php include "./components/inner-contact.php"?>
+<!-- Contact Section -->
+
+<!-- Footer Section -->
+<?php include "./components/footer.php"?>
+<!-- Footer Section -->
+
+<script src="/assets/js/jquery.js"></script>
+<script src="/assets/js/custom.js"></script>
+<script>
+    new WOW().init();
+</script>
+<script src="https://cdn.lordicon.com/lordicon.js"></script>
+</body>
+</html>
